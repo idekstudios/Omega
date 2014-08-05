5f81d361dce24d4cd3bc245af64601ae082e5932
bkisby <bryankisby@Bryans-MacBook-Pro.local>
2014-08-04 21:14:22 -0400

    Project Creation
    First submit to test if unity settings are correct

---
+++ OmegaUnity/Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("This is a test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
